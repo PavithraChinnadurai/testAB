0c9992aee8173e38e249e01fe58b382a2d55801f
Pavithra Chinnadurai <pavithravincent@gmail.com>
2021-09-15 18:51:10 +0530

    Added a platform

--- sketch.js
+++ sketch.js
@@ -4,14 +4,15 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-
+var platform;
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform=new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -54,4 +55,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
